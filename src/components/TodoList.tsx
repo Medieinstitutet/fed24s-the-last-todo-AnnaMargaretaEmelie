94363adcd8269ff5c1a74fea87599ad2304d1e67
Anna <anna.viklund1@medieinstitutet.se>
2025-05-07 17:17:05 +0200

    Made tasks move to a different section when done.

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -8,16 +8,39 @@ type TodoProps = {
 };
 
 export function TodoList({ todos, toggleDone, deleteTodo }: TodoProps) {
+  const showTodos = (
+    text: string,
+    todosToShow: Todo[],
+    emptyMessage: string
+  ) => (
+    <section>
+      <h2>{text}</h2>
+      {todosToShow.length > 0 ? (
+        <ul>
+          {todosToShow.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              toggleDone={toggleDone}
+              deleteTodo={deleteTodo}
+            />
+          ))}
+        </ul>
+      ) : (
+        <p>{emptyMessage}</p>
+      )}
+    </section>
+  );
+
+  const activeTodos = todos.filter((todo) => !todo.done);
+  const completedTodos = todos.filter((todo) => todo.done);
+
   return (
-    <ul>
-      {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          toggleDone={toggleDone}
-          deleteTodo={deleteTodo}
-        />
-      ))}
-    </ul>
+    <>
+      <div>
+        {showTodos("Att göra", activeTodos, "Inga uppgifter kvar att göra!")}
+        {showTodos("Klara", completedTodos, "Inga klara uppgifter än")}
+      </div>
+    </>
   );
 }
